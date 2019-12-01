82dea8a9338099839a786fb47d6bf7cd74e1a308
Seandre <seanandrepinzon@gmail.com>
2019-11-30 16:14:59 -0800

    build works now

--- src/SimpleAPI/Controllers/ValuesController.cs
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -17,7 +17,7 @@ namespace SimpleAPI.Controllers
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "notSean";
+            return "Sean";
         }
 
         // POST api/values
